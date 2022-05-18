68456ea0e1706252d1c627e15447ba5fd1f81d1a
KevinGuzman <kguzman077@gmail.com>
2022-05-17 21:37:43 -0600

    Week 04 updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { MessageItemComponent } from './messages/message-item/message-item.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
-
+import { DropdownDirective } from './shared/dropdown.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,6 +32,7 @@ import { MessageEditComponent } from './messages/message-edit/message-edit.compo
     MessageListComponent,
     MessageItemComponent,
     MessageEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    new Document(
+      1,
+      'CIT 260 - Object Oriented Programming',
+      'Document 1',
+      'www.byui.edu',
+      null
+    ),
+    new Document(
+      2,
+      'WDD 430 - Web Full Stack Development',
+      'Document 2',
+      'www.byui.edu',
+      null
+    ),
+    new Document(
+      3,
+      'CIT 420 - Data Warehousing',
+      'Document 3',
+      'www.byui.edu',
+      null
+    ),
+    new Document(
+      4,
+      'CIT 460 - Enterpise Development',
+      'Document 4',
+      'www.byui.edu',
+      null
+    ),
+    new Document(
+      5,
+      'CIT 495 - Senior Project',
+      'Document 5',
+      'www.byui.edu',
+      null
+    ),
+    new Document(
+      6,
+      'Web Developer Full Stack',
+      'Kevin Guzman 6',
+      'www.github.com/kjg1993',
+      null
+    ),
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
-
 }
